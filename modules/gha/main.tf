07706379a003fab1e681928f1d942c5e0be57290
ryota matsushita <ryotinjpn@gmail.com>
2025-03-25 18:47:44 +0900

    ✨ ghaモジュールを作成

---
+++ modules/gha/main.tf
@@ -0,0 +1,23 @@
+# -------------------------------------
+# IAM
+# -------------------------------------
+resource "aws_iam_openid_connect_provider" "main" {
+  url            = "https://token.actions.githubusercontent.com"
+  client_id_list = ["sts.amazonaws.com"]
+}
+
+resource "aws_iam_role" "main" {
+  name               = "GHAPRAgentRole-${var.project_name}-${var.environment}"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_policy" "main" {
+  name        = "GHAPRAgentAccessForBedrock-${var.project_name}-${var.environment}"
+  path        = "/service-role/"
+  policy      = data.aws_iam_policy_document.bedrock.json
+}
+
+resource "aws_iam_role_policy_attachment" "main" {
+  role       = aws_iam_role.main.name
+  policy_arn = aws_iam_policy.main.arn
+}
---
+++ modules/gha/variables.tf
@@ -0,0 +1,18 @@
+# -------------------------------------
+# Common
+# -------------------------------------
+variable "project_name" {
+  type        = string
+  description = "Enter the project name."
+  nullable    = false
+}
+
+variable "environment" {
+  description = "Enter the environment."
+  type        = string
+  nullable    = false
+  validation {
+    condition     = contains(["sandbox", "dev", "stg", "prd"], var.environment)
+    error_message = "environment must be one of the following: [sandbox, dev, stg, prd]."
+  }
+}
