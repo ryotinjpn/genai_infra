e79610fbc7ac443b1db1de63a499d579998d16cb
ryota_matsushita <matsushita@ww-system.com>
2025-03-30 00:51:40 +0900

    👍 オンデマンドモードに変更

--- modules/dynamodb/main.tf
+++ modules/dynamodb/main.tf
@@ -3,7 +3,7 @@
 # -------------------------------------
 resource "aws_dynamodb_table" "generate_post_to_x" {
   name           = "GeneratePostToXHistory"
-  billing_mode   = "PROVISIONED"
+  billing_mode   = "PAY_PER_REQUEST"
   read_capacity  = 1
   write_capacity = 1
   hash_key       = "HistoryId"
