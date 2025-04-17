bae5f64765c1f7d3d3c9066145593c3dfd118aac
ryota matsushita <ryotinjpn@gmail.com>
2025-04-17 23:38:16 +0900

    ♻️ 同一モジュール間でメソッド呼び出しに変更

--- functions/generate_post_to_x/src/lambda_handler.py
+++ functions/generate_post_to_x/src/lambda_handler.py
@@ -11,8 +11,7 @@ def lambda_handler(event, _):
     try:
         post_history = dynamodb_resource.get_post_history()
         new_post = bedrock.generate_post(post_history)
-        access_token = x_api.get_x_access_token()
-        x_api.create_x_to_posts(access_token, new_post)
+        x_api.create_x_to_posts(new_post)
         dynamodb_resource.put_post_history(new_post)
 
         return {"status_code": 200, "message": "処理成功"}
