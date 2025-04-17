10d2f4e626a85a1bb9ba7764a207afb96ee37a75
ryota matsushita <ryotinjpn@gmail.com>
2025-04-18 00:06:00 +0900

    ✨ LINE API Webhookから送信先IDを取得する Lambdaを作成

---
+++ functions/save_target_id_to_line/src/lambda_handler.py
@@ -0,0 +1,26 @@
+import json
+import ssm
+
+
+def lambda_handler(event, _):
+    """Lambdaエントリーポイント"""
+
+    try:
+        body = json.loads(event["body"])
+        source_type = body['events'][0]['source']['type']
+
+        match source_type:
+            case "user":
+                target_id = body['events'][0]['source']['userId']
+            case "group":
+                target_id = body['events'][0]['source']['groupId']
+            case "room":
+                target_id = body['events'][0]['source']['roomId']
+            case _:
+                return {"status_code": 400, "message": "不正なソースタイプ"}
+        
+        ssm.update_ssm_parameter(target_id)
+
+        return {"status_code": 200, "message": "処理成功"}
+    except Exception as e:
+        return {"status_code": 500, "message": "処理失敗", "error": str(e)}
