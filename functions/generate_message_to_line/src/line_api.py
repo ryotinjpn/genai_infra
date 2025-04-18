e61fc7db357b91473ac592a4f94b962d981690be
ryota matsushita <ryotinjpn@gmail.com>
2025-04-18 12:05:33 +0900

    👍 docstringを修正

--- functions/generate_message_to_line/src/line_api.py
+++ functions/generate_message_to_line/src/line_api.py
@@ -12,7 +12,7 @@ def push_line_to_messages(messages: str):
     """LINEへプッシュメッセージを送信する
 
     Args:
-        new_post (str): 投稿するポスト内容
+        push_line_to_ (str): 送信内容
     """
 
     channel_access_token = ssm.get_parameter_store_value(
