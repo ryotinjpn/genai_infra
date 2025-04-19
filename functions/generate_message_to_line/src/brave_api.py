00aaf66f88d31e9b95dcea3dc12d4e232bce9642
ryota matsushita <ryotinjpn@gmail.com>
2025-04-19 15:21:39 +0900

    ♻️ 整形処理を別ファイルに移動

--- functions/generate_message_to_line/src/brave_api.py
+++ functions/generate_message_to_line/src/brave_api.py
@@ -1,101 +1,10 @@
 import logging
 
-import jmespath
 import requests
-from bs4 import BeautifulSoup
+from transformer import cleaned_html
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-
-DEL_HTML_TAG_LIST = [
-    "script",
-    "style",
-    "nav",
-    "header",
-    "footer",
-    "aside",
-    "noscript",
-    "iframe",
-    "img",
-    "svg",
-    "path",
-]
-
-DEL_HTML_CLASS_LIST = [
-    "ads",
-    "ad-",
-    "banner",
-    "menu",
-    "nav",
-    "sidebar",
-    "footer",
-    "comments",
-    "related",
-]
-
-DEL_HTML_ID_LIST = [
-    "ads",
-    "ad-",
-    "banner",
-    "menu",
-    "nav",
-    "sidebar",
-    "footer",
-    "comments",
-    "related",
-]
-
-
-def cleaned_html(html_json):
-    """HTML整形処理をする
-
-    Args:
-        html_json (json): 整形するHTMLのjson
-
-    Returns:
-        list: 整形後のHTML
-    """
-
-    urls = jmespath.search("web.results[*].url", html_json)
-    cleaned_htmls = []
-    logger.info("HTML整形処理開始")
-    for url in urls:
-        print(url)
-        response = requests.get(url)
-        response.raise_for_status()
-        response.encoding = "utf-8"
-
-        # BeautifulSoupでHTMLを解析
-        soup = BeautifulSoup(response.text, "html.parser")
-
-        # bodyタグを取得
-        body_tag = soup.body
-
-        if body_tag:
-            body_soup = BeautifulSoup(str(body_tag), "html.parser")
-
-            for tag in body_soup.find_all(DEL_HTML_TAG_LIST):
-                tag.decompose()
-
-            for class_name in DEL_HTML_CLASS_LIST:
-                for tag in body_soup.find_all(
-                    class_=lambda x: x and class_name in x.lower()
-                ):
-                    tag.decompose()
-
-            for id_name in DEL_HTML_ID_LIST:
-                for tag in body_soup.find_all(id=lambda x: x and id_name in x.lower()):
-                    tag.decompose()
-            cleaned_html = body_soup.get_text(separator="\n", strip=True)
-        else:
-            # bodyタグがない場合はHTMLの全テキストを返す
-            for tag in soup.find_all(["script", "style"]):
-                tag.decompose()
-            cleaned_html = soup.get_text(separator="\n", strip=True)
-        cleaned_htmls.append(cleaned_html)
-
-    logger.info("HTML整形処理完了")
-    return cleaned_htmls
 
 
 def search_brave(brave_api_key: str, keyword: str):
@@ -116,7 +25,7 @@ def search_brave(brave_api_key: str, keyword: str):
     }
     params = {
         "q": keyword,
-        "count": 3,
+        "count": 1,
         "text_decorations": 0,
         "country": "JP",
         "search_lang": "jp",
@@ -124,7 +33,7 @@ def search_brave(brave_api_key: str, keyword: str):
     try:
         logger.info("検索開始")
         response = requests.get(
-            f"https://api.search.brave.com/res/v1/web/search",
+            "https://api.search.brave.com/res/v1/web/search",
             headers=headers,
             params=params,
         )
