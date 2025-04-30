e7b373061d4e4c25e14c54604d673d72b3fe1b27
ryota matsushita <ryotinjpn@gmail.com>
2025-04-30 22:49:34 +0900

    ✨ lambda_invoke_geminiモジュールを作成

---
+++ modules/lambda_invoke_gemini/data.tf
@@ -0,0 +1,85 @@
+data "aws_caller_identity" "self" {}
+data "aws_region" "current" {}
+data "aws_ssm_parameter" "profile_name" {
+  name = "PROFILE_NAME"
+}
+
+#######################################
+# ECR
+#######################################
+data "aws_ecr_lifecycle_policy_document" "main" {
+  rule {
+    priority    = 1
+    description = "life cycle for untagged images"
+    selection {
+      tag_status   = "untagged"
+      count_type   = "imageCountMoreThan"
+      count_number = 1
+    }
+    action {
+      type = "expire"
+    }
+  }
+}
+
+#######################################
+# IAM
+#######################################
+data "aws_iam_policy_document" "lambda_assume_role" {
+  version = "2012-10-17"
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+data "aws_iam_policy_document" "lambda_role" {
+  version = "2012-10-17"
+  statement {
+    effect = "Allow"
+    actions = [
+      "ssm:GetParameter",
+      "ssm:GetParameters",
+    ]
+    resources = [
+      "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.self.id}:parameter/*",
+    ]
+  }
+  statement {
+    effect = "Allow"
+    actions = [
+      "kms:Decrypt",
+      "kms:Encrypt",
+      "kms:GenerateDataKey",
+    ]
+    resources = [
+      "arn:aws:kms:${data.aws_region.current.name}:${data.aws_caller_identity.self.id}:key/*"
+    ]
+  }
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+    resources = ["arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.self.id}:log-group:/aws/lambda/${aws_lambda_function.main.function_name}:*"]
+  }
+  statement {
+    effect = "Allow"
+    actions = [
+      "sns:Publish",
+    ]
+    resources = [var.sns_topic_arn]
+  }
+  statement {
+    effect = "Allow"
+    actions = [
+      "sts:AssumeRoleWithWebIdentity",
+    ]
+    resources = ["arn:aws:iam::${data.aws_caller_identity.self.id}:role/*"]
+  }
+}
---
+++ modules/lambda_invoke_gemini/main.tf
@@ -0,0 +1,94 @@
+#######################################
+# Lambda Function
+#######################################
+resource "aws_lambda_function" "main" {
+  function_name = "${var.project_name}-${var.environment}-invoke-gemini"
+  description   = "Gemini 呼び出し用 Lambda 関数"
+  package_type  = "Image"
+  image_uri     = "${aws_ecr_repository.main.repository_url}:latest"
+  memory_size   = 512
+  timeout       = 900
+  role          = aws_iam_role.main.arn
+  environment {
+    variables = {
+      TZ                                  = "Asia/Tokyo"
+      GOOGLE_APPLICATION_CREDENTIALS_PATH = "/${var.project_name}/${var.environment}/GOOGLE_APPLICATION_CREDENTIALS"
+      GCP_PROJECT_ID                      = var.gcp_project_id
+      SNS_TOPIC_ARN                       = var.sns_topic_arn
+    }
+  }
+
+  # ECRリポジトリにイメージがプッシュされた後に実行
+  depends_on = [null_resource.docker_push]
+}
+
+#######################################
+# ECR
+#######################################
+resource "aws_ecr_repository" "main" {
+  name                 = "${var.project_name}/lambda_invoke_gemini"
+  image_tag_mutability = "MUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+  force_delete = true
+}
+
+resource "aws_ecr_lifecycle_policy" "main" {
+  repository = aws_ecr_repository.main.name
+  policy     = data.aws_ecr_lifecycle_policy_document.main.json
+}
+
+resource "null_resource" "docker_push" {
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+  provisioner "local-exec" {
+    command = <<EOT
+      aws ecr get-login-password --region ${data.aws_region.current.name} --profile ${data.aws_ssm_parameter.profile_name.value} | docker login --username AWS --password-stdin ${aws_ecr_repository.main.repository_url}
+      docker tag ${aws_ecr_repository.main.name}:latest ${aws_ecr_repository.main.repository_url}:latest
+      docker push ${aws_ecr_repository.main.repository_url}:latest
+    EOT
+  }
+  # ECRリポジトリの作成後に実行
+  depends_on = [aws_ecr_repository.main]
+}
+
+resource "null_resource" "lambda_update_function_code" {
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+  provisioner "local-exec" {
+    command = <<EOT
+      aws lambda update-function-code --function-name ${aws_lambda_function.main.function_name} --image-uri ${aws_ecr_repository.main.repository_url}:latest --region ${data.aws_region.current.name} --profile ${data.aws_ssm_parameter.profile_name.value}
+    EOT
+  }
+  # Lambda関数の作成後に実行
+  depends_on = [aws_lambda_function.main]
+}
+
+#######################################
+# CloudWatch Logs
+#######################################
+resource "aws_cloudwatch_log_group" "main" {
+  name              = "/aws/lambda/${aws_lambda_function.main.function_name}"
+  retention_in_days = 30
+}
+
+#######################################
+# IAM
+#######################################
+resource "aws_iam_role" "main" {
+  name               = "LambdaRoleForInvokeGemini-${var.project_name}-${var.environment}"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "main" {
+  role       = aws_iam_role.main.name
+  policy_arn = aws_iam_policy.main.arn
+}
+
+resource "aws_iam_policy" "main" {
+  name   = "LambdaRoleForInvokeGemini-${var.project_name}-${var.environment}"
+  policy = data.aws_iam_policy_document.lambda_role.json
+}
